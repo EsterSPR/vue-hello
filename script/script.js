882721e3e7fa3ec99341700053c86f8a60c9f01a
EsterSPR <web.esterspreafico@gmail.com>
2022-12-19 16:10:21 +0100

    completato compito con anche immagine

--- script/script.js
+++ script/script.js
@@ -3,12 +3,9 @@ const { createApp } = Vue
 createApp({
     data() {
         return{
-            word: null
+            nome: 'Alberto',
+            cognome: 'Angela',
+            immagine: 'https://images2.corriereobjects.it/methode_image/2018/11/24/Interni/Foto%20Interni%20-%20Trattate/Alberto%20Angela%202ttcredit%20Barbara%20Ledda-khbD-U307042592216kb-1224x916@Corriere-Web-Sezioni-593x443.jpg?v=20181125153444'
         }
-    },
-    methods: {
-        greetings: function() {
-            this.word;
-        },
     }
 }).mount('#app')
